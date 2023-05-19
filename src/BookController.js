a10965fe213fb58d0f435fe50b2a1fe06680f58a
Vova <vpr@ggr.group>
2023-05-19 18:41:53 +0300

    Initial commit

---
+++ src/BookController.js
@@ -0,0 +1,20 @@
+angular.module('bookApp')
+    .controller('BookController', function() {
+        var vm = this;
+        vm.books = [
+            { title: 'Title1', author: 'Author1', genre: 'Genre1' },
+            { title: 'Title2', author: 'Author2', genre: 'Genre2' },
+            { title: 'Title3', author: 'Author3', genre: 'Genre3' }
+        ];
+
+        vm.newBook = {};
+
+        vm.addBook = function() {
+            vm.books.push(vm.newBook);
+            vm.newBook = {};
+        };
+
+        vm.removeBook = function(index) {
+            vm.books.splice(index, 1);
+        };
+    });
